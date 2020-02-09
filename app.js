98166834bb394e7258ef12a48e7edefbc90753e9
Ron Norasak <ronnorasak@gmail.com>
2020-02-09 15:07:58 -0600

    up to first step of making queries

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const routes = require('./routes/pages');
 const models = require('./models');
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', routes);
 
 const PORT = 3000;
 
-const init = async() => {
-  await models.User.sync();
-  await models.Page.sync();
-  console.log ('synced!');
+const init = async () => {
+  await models.db.sync({ force: true });
+  console.log('synced!');
   app.listen(PORT, () => {
     console.log(`Server listening in port ${PORT}`);
   });
 };
 
 init();
-
